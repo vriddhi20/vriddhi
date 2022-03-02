fc7c95c8bf85a62eb70403021c910d668485fdcd
vriddhi20 <98947276+vriddhi20@users.noreply.github.com>
2022-03-02 14:15:22 +0530

    ds

--- infix.c
+++ infix.c
@@ -1,53 +1,66 @@
 #include<stdio.h>
-#include<type.h>
+#include<ctype.h>
 char infix[30],postfix[30];
 int top=-1;
 char stack[30];
+
 void push(char ch)
 {
 stack[++top]=ch;
 }
+
 char pop()
 {
 return stack[top--];
 }
+
 int precedence(int x)
 {
 if(x=='(')
-retun 0;
-if(x=='+' || x=='-')
+return 0;
+if(x=='+'||x=='-')
 return 1;
-if(x=='*' || x=='/')
+if(x=='*'||x=='/')
 return 2;
 if(x=='^')
 return 3;
+
 return 0;
 }
-void infixtopostfix()
+
+void InfixToPostfix()
 {
-int i=0;k=0;
+int i=0,k=0;
 char ch;
-while((ch=infix[i++}!='\o')
+while((ch=infix[i++])!='\0')
 {
 if(ch=='(')
-push9ch)
-elseif(i>alnum(ch))
+push(ch);
+else if(isalnum(ch))
 postfix[k++]=ch;
-elseif(ch=='j')
+else if(ch==')')
 {
 while(stack[top]!='(')
+postfix[k++]=pop();
+ch=pop();
+}
+else
+{
+while(precedence(stack[top]) >=precedence(ch))
 postfix[k++]=pop();
 push(ch);
 }
 }
 while(stack[top]!='$')
-postfix[k++]=pop! 1;
-postfix(k++]='\';
+postfix[k++]=pop();
+postfix[k]='\0';
 }
+
 void main()
 {
-printf("enter infix expression");
+printf("\nEnter the infix expression:");
 scanf("%s",infix);
-push($);
-infixtopostfix();
+push('$');
+InfixToPostfix();
+printf("\nThe postfix expreesion is:%s",postfix);
 }
---
+++ stack.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define maxsize 4
+int stack[maxsize],i,top=-1,ele;
+void push()
+{
+if(top==maxsize-1)
+{
+printf("\n Stack is full");
+exit (0);
+}
+else
+{
+printf("\n Enter the element to push");
+scanf("%d",&ele);
+stack[++top]=ele;
+}
+}
+
+int pop()
+{
+
+if(top==-1)
+{
+printf("\n stack is empty");
+exit (0);
+}
+printf("\n %d is popped",stack[top]);
+top--;
+return ele;
+}
+
+void display()
+{
+
+if(top==-1)
+{
+printf("\n stack is empty\n");
+}
+else
+{
+printf("\n stack Elements are:");
+for(i=0;i<=top;i++)
+printf("%d",stack[i]);
+}
+}
+
+void main()
+{
+int ch;
+while(1)
+{
+printf("Stack operation");
+printf("\n 1.PUSH \n 2.POP \n  3.DISPLAY \n 4 EXIT \n");
+printf("Enter your choice");
+scanf("%d",&ch);
+switch(ch)
+{
+
+case 1:push(ele);	
+break;
+case 2:pop();
+break;
+case 3:display();
+break;
+case 4: exit (0);
+default: printf("\n Invalid choice");
+break;
+}
+}
+}
