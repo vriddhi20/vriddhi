971e80a50013fded0778d590a5b4ba8267385625
vriddhi20 <98947276+vriddhi20@users.noreply.github.com>
2022-04-06 16:32:01 +0530

    unix

---
+++ 9b.sh
@@ -0,0 +1,35 @@
+echo -n "enter a filename"
+read filename
+if [ -f $filename ]
+then
+	n=40
+	while read line
+	do
+		ll=`expr length "$line"`
+		if [ $ll -le $n ]
+		then
+			echo $line
+		else
+			s=1
+			e=$n
+			echo "`echo $line | cut -c $s-$e` \\"
+			rcl=`expr $ll - $e`
+			while [ $rcl -ge $n ]
+			do
+				s=`expr $e + 1`
+				e=`expr $e + $n`
+				echo "`echo $line | cut -c $s-$e` \\"
+				rcl=`expr $ll - $e`
+			done
+			s=`expr $e + 1`
+		echo $line | cut -c $s-
+		fi
+	done<$filename
+else
+	echo "$filename :no such file found"
+fi
+
+
+
+
+
