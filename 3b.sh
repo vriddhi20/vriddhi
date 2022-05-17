fff5b105b507693f6a4680c245536ef581837db1
vriddhi20 <98947276+vriddhi20@users.noreply.github.com>
2022-05-17 23:26:24 +0530

    Delete 3b.sh

--- 3b.sh
+++
@@ -1,9 +0,0 @@
-date=`date +%d`
-date=`expr + 0`
-l=`cal | tr -d "[\137][\010]" | grep -wn "$d" | head -c 1`
-if [ $d -gt 9 ]
-then
-	cal | tr -d "[/137][\010]" | sed "$l s/$d/**/"
-else
-	cal | tr -d "[/137][\010]" | sed "$l s/$d/*/"
-fi
